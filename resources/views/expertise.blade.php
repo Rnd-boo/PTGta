ac98845b87acc581b050bdf41af9c5027fc4040f
Randy <rand4035@gmail.com>
2025-01-10 20:27:21 +0700

    Finish our expertise section

--- resources/views/expertise.blade.php
+++ resources/views/expertise.blade.php
@@ -14,20 +14,29 @@
     <x-navbar></x-navbar>
     <section class="bg-white">
         <div>
-          <div class="relative h-[36rem] w-full bg-[url('/public/img/Our-expertise.jpg')] bg-cover bg-no-repeat lg:bg-center">
-            <h1 class="absolute bottom-0 px-24 py-14 text-3xl font-black text-white lg:text-6xl">OUR EXPERTISE</h1>
+          <div class="relative h-[36rem] w-full bg-[url('/public/img/Our-expertise.jpg')] bg-cover bg-no-repeat bg-center">
+            <h1 class="absolute bottom-0 px-16 lg:px-24 py-14 text-4xl font-black text-white lg:text-6xl">OUR EXPERTISE</h1>
           </div>
         </div>
-        <div class="mx-auto mt-10 xl:max-w-screen-xl 2xl:max-w-screen-2xl grid grid-cols-1 lg:grid-cols-2">
+        <div class="mx-auto mt-10 xl:max-w-screen-xl 2xl:max-w-screen-2xl  grid grid-cols-1 lg:grid-cols-2 px-4">
             <img src="img/filter-press.jpg" alt="Build-Filter-press" class="h-100 w-full object-cover object-center" />
-            <h1 class="content-center bg-red-900 p-8 text-6xl text-white">Build - Installation</h1>
-            <h1 class="content-center p-8 text-6xl text-orange-400">Modification - Renovation</h1>
-            <img src="img/renovation.png" alt="" class="h-100 w-full object-cover object-center" />
-            <img src="img/repair.png" alt="" class="h-100 w-full object-cover object-center" />
-            <h1 class="content-center bg-red-900 p-8 text-6xl text-white">Repair - Restoration</h1>
+            <div class="content-center bg-red-900 p-8">
+              <h1 class="mt-8 lg:mt-12 text-3xl lg:text-6xl text-white">Build - Installation</h1>
+              <p class="my-8 lg:mt-12 text-white"><a href="/404" class="border-2 border-orange-400 w-max rounded-full px-4 py-1 text-sm lg:text-lg hover:bg-orange-400 hover:text-black">Learn More</a></p>
+            </div>
+            <div class="content-center p-8 order-2 lg:order-none">
+              <h1 class="mt-8 lg:mt-12 text-3xl lg:text-6xl text-red-600">Renovation</h1>
+              <p class="my-8 lg:mt-12"><a href="/404" class="border-2 border-red-600 w-max rounded-full px-4 py-1 text-sm lg:text-lg hover:bg-red-600 hover:text-white">Learn More</a></p>
+            </div>
+            <img src="img/renovation.png" alt="" class="h-100 w-full object-cover object-center order-1 lg:order-none" />
+            <img src="img/repair.png" alt="" class="h-100 w-full object-cover object-center order-3 lg:order-none" />
+            <div class="content-center bg-red-900  p-8 order-4 lg:order-none">
+              <h1 class="mt-8 lg:mt-12 text-3xl lg:text-6xl text-white">Repair - Restoration</h1>
+              <p class="my-8 lg:mt-12 text-white"><a href="/404" class="border-2 border-orange-400 w-max rounded-full px-4 py-1 text-sm lg:text-lg hover:bg-orange-400 hover:text-black">Learn More</a></p>
+            </div>
         </div>
         <div class="my-20">
-            <h1 class="text-center text-lg">Don't Hestitate to <a href="" class="focus:ring-primary-300 inline-flex items-center rounded-lg bg-orange-600 px-2.5 pb-1 text-white hover:bg-orange-800 focus:ring-4">Contact Us</a> for further information</h1>
+            <h1 class="text-center text-base lg:text-lg">Don't Hestitate to <a href="" class="focus:ring-primary-300 inline-flex items-center rounded-lg bg-orange-600 px-3 py-1 text-white hover:bg-orange-800 focus:ring-4">Contact Us</a> for further information</h1>
           </div>
       </section>
       <x-footer></x-footer>
