5c58bce9ca35d9d6fa42deafb338974f5e0bcec4
Randy <rand4035@gmail.com>
2025-01-15 16:09:15 +0700

    Finish About Company

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,1 +1,72 @@
-<x-layout></x-layout>
+<x-layout>
+    <div>
+        <div class="flex justify-end overflow-x-hidden">
+          <div class="z-0 mt-10 w-1/2 translate-x-32 text-red-900">
+            <h1 class="text-2xl font-semibold">ABOUT OUR</h1>
+            <h1 class="mt-1 text-5xl font-bold">COMPANY</h1>
+            <div class="w-full bg-red-900">
+              <p class="mt-8 w-full max-w-2xl p-4 py-5 text-2xl text-white">PT.GEMA TRIMITRA ANDALAN <br />A company founded by someone with 20 years of experience in the construction field. We build not only building, but also trust. With innovation and integrity, we realize high-quality sustainable projects.</p>
+            </div>
+            <!-- <p class="text-orange-500 bg-red-900 p-4 w-full text-2xl pr-36 mt-8 max-w-2xl">PT.GEMA TRIMITRA ANDALAN <br>A company founded by someone with 20 years of experience in the construction field. We build not only building, but also trust. With innovation and integrity, we realize high-quality sustainable projects.</p> -->
+          </div>
+          <img src="https://img.freepik.com/free-photo/view-modern-construction-site_23-2151317369.jpg?t=st=1736565115~exp=1736568715~hmac=87e59885218ebc569cd0d47346a84f4120436a3bcdbe7206822aa5cdba900192&w=740" alt="" class="z-10 max-w-4xl translate-x-20 -skew-x-12" />
+        </div>
+        <div class="mx-auto grid max-w-7xl grid-cols-1 gap-8 lg:grid-cols-2 my-10 mt-20">
+          <div>
+            <h1 class="p-2 text-red-900 font-medium text-3xl">Our Mission</h1>
+            <ul class="list-disc list-inside bg-red-900 p-4 rounded-xl text-white">
+              <li>Quality : "Build to high standards"</li>
+              <li>Customer : "Customer satisfaction number one"</li>
+              <li>Team : "Solid, professional team"</li>
+              <li>Innovation : "Continue to innovate, excel in the market"</li>
+            </ul>
+          </div>
+          <div>
+            <h1 class="p-2 text-red-900 font-medium text-3xl">Our Vision</h1>
+            <ul class="list-disc list-inside bg-red-900 p-4 rounded-xl text-white">
+              <li>Focus on quality: "Quality building, satisfied customers."</li>
+              <li>Customer-oriented: "Trusted construction solutions, always on time."</li>
+              <li>Sustainable: "Green construction, bright future."</li>
+              <li>Innovation: "Leading technology, best work."</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="mx-auto flex max-w-7xl my-10">
+        <h1 class="text-3xl mr-6 p-4 text-red-900 font-medium">Our Office on <a href="https://www.google.com/maps/place/PT.+Gema+Trimitra+Andalan/@3.656214,98.66391,16z/data=!4m6!3m5!1s0x303133004785627f:0x85f275325399b872!8m2!3d3.6562141!4d98.6639103!16s%2Fg%2F11vyc0fx1j?hl=en&entry=ttu&g_ep=EgoyMDI1MDExMC4wIKXMDSoASAFQAw%3D%3D" class="underline text-center font-normal text-xl hover:text-gray-500">Google Maps</a> </h1>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3981.6975918718745!2d98.66133537585904!3d3.6562194498245413!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x303133004785627f%3A0x85f275325399b872!2sPT.%20Gema%20Trimitra%20Andalan!5e0!3m2!1sen!2sid!4v1736930157898!5m2!1sen!2sid" width="800" height="400" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      </div>
+      {{-- Our Client section --}}
+    {{-- <h1 class="mx-auto mt-24 mb-4 text-center text-2xl lg:text-5xl font-semibold">Our Client</h1> --}}
+    <div class="mx-auto w-full pb-5 my-4">
+        <div class="overflow-hidden flex group">
+          <ul class="flex text-black p-4 whitespace-nowrap text-md lg:text-xl font-medium animate-loop-scroll space-x-16 group-hover:paused">
+            <li class="cursor-pointer transition-all h-2 hover:scale-105 hover:text-red-700">PT. ENERGI OLEO PERSADA</li>
+            <li class="cursor-pointer transition-all h-2 hover:scale-105 hover:text-red-500">PT. SOCIMAS</li>
+            <li class="cursor-pointer transition-all h-2 hover:scale-105 hover:text-red-600">PT. EXPRAVET NASUBA.</li>
+            <li class="cursor-pointer transition-all h-2 hover:scale-105 hover:text-orange-500">PT. INTIMAS SAWIT SEJAHTERA.</li>
+            <li class="cursor-pointer transition-all h-2 hover:scale-105 hover:text-green-700">PT. MUSIM MAS.</li>
+            <li class="cursor-pointer transition-all h-2 hover:scale-105 hover:text-blue-400">PT. TRIMITRA PROTEIN INDONESIA.</li>
+            <li class="cursor-pointer transition-all h-2 hover:scale-105 hover:text-red-600">PT. MABAR FEED INDONESIA.</li>
+            <li class="cursor-pointer transition-all h-2 hover:scale-105 hover:text-green-600">PT. SAWIT RUPAT SEJAHTERA.</li>
+            <li class="cursor-pointer transition-all h-2 hover:scale-105">PT. SERDANG JAYA PERDANA.</li>
+            <li class="cursor-pointer transition-all h-2 hover:scale-105 hover:text-red-600">PT. SURYA TATA MANDIRI.</li>
+            <li class="cursor-pointer transition-all h-2 hover:scale-105 hover:text-blue-600">PT. ENVI TECH</li>
+          </ul>
+          <ul class="flex text-black p-4 whitespace-nowrap text-xl font-medium animate-loop-scroll space-x-16 group-hover:paused" aria-hidden="true">
+            <li class="cursor-pointer transition-all h-2 hover:scale-105 hover:text-red-700">PT. ENERGI OLEO PERSADA</li>
+            <li class="cursor-pointer transition-all h-2 hover:scale-105 hover:text-red-500">PT. SOCIMAS</li>
+            <li class="cursor-pointer transition-all h-2 hover:scale-105 hover:text-red-600">PT. EXPRAVET NASUBA.</li>
+            <li class="cursor-pointer transition-all h-2 hover:scale-105 hover:text-orange-500">PT. INTIMAS SAWIT SEJAHTERA.</li>
+            <li class="cursor-pointer transition-all h-2 hover:scale-105 hover:text-green-700">PT. MUSIM MAS.</li>
+            <li class="cursor-pointer transition-all h-2 hover:scale-105 hover:text-blue-400">PT. TRIMITRA PROTEIN INDONESIA.</li>
+            <li class="cursor-pointer transition-all h-2 hover:scale-105 hover:text-red-600">PT. MABAR FEED INDONESIA.</li>
+            <li class="cursor-pointer transition-all h-2 hover:scale-105 hover:text-green-600">PT. SAWIT RUPAT SEJAHTERA.</li>
+            <li class="cursor-pointer transition-all h-2 hover:scale-105">PT. SERDANG JAYA PERDANA.</li>
+            <li class="cursor-pointer transition-all h-2 hover:scale-105 hover:text-red-600">PT. SURYA TATA MANDIRI.</li>
+            <li class="cursor-pointer transition-all h-2 hover:scale-105 hover:text-blue-600">PT. ENVI TECH</li>
+          </ul>
+        </div>
+    </div>
+    {{-- End Our Client section --}}
+</x-layout>
