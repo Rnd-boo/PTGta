e9f1c8aa01d588281ed3f709047e47eb7b8abbb1
Randy <rand4035@gmail.com>
2025-01-22 19:18:06 +0700

    pull from github

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -26,7 +26,7 @@
       <h1 class="text-5xl font-semibold my-10 mt-5 md:mt-20">Connect with Us</h1>
       <p class="">We're on <a href="" class="hover:text-gray-400 cursor-pointer underline text-gray-700">WhatsApp</a> if you don't like forms. <br>And acceptin emails at <a href="" class="hover:text-gray-400 cursor-pointer underline text-gray-700">gematrimitraandalan@my.com</a></p>
     </div>
-  <form id="contact-form" class="mx-auto max-w-md mt-10" method="" autocomplete="off">
+  <form id="contact-form" class="mx-auto max-w-md mt-10" method="GET" autocomplete="off">
   <div class="group relative z-0 mb-5 w-full">
     <input type="email" name="email" id="floating_email" class="peer block w-full appearance-none border-0 border-b-2 border-gray-300 bg-transparent px-0 py-2.5 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0 " placeholder=" " required />
     <label for="floating_email" class="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:start-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:font-medium peer-focus:text-blue-600 rtl:peer-focus:left-auto rtl:peer-focus:translate-x-1/4  peer-focus:dark:text-blue-500">Email address</label>
