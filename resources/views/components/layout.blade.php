c5e4b5fe43bfd7b34c2664a15f90c8f2ebdbbcd7
Randy <rand4035@gmail.com>
2025-01-22 19:44:33 +0700

    add meta contentsecuritypolicy

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,10 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     @vite('resources/css/app.css')
-    <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
+    <link rel="stylesheet" href="{{ asset('https://rsms.me/inter/inter.css') }}">
     <link rel="shortcut icon" href="img/logo.png" />
-    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    <script defer src="{{ asset('https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js') }}"></script>
     <title>PT.Gema Trimitra Andalan</title>
 </head>
 <body class="flex flex-col min-h-screen">
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     @vite('resources/css/app.css')
     <link rel="stylesheet" href="{{ asset('https://rsms.me/inter/inter.css') }}">
     <link rel="shortcut icon" href="img/logo.png" />
